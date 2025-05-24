866a4b73bd3f2c16579c58be3799b6763dcd9b58
karan601 <130557323+karan601@users.noreply.github.com>
2025-05-24 21:51:36 +0530

    Check for 3 matching emojis in a line to determine winner

---
+++ src/utils/helpers.js
@@ -0,0 +1,23 @@
+export const checkWinner = (board, player) => {
+    const checkLine = (cells) =>
+      cells.every(
+        ([r, c]) => board[r][c] && board[r][c].player === player
+      );
+  
+    const lines = [
+      // Rows
+      [[0, 0], [0, 1], [0, 2]],
+      [[1, 0], [1, 1], [1, 2]],
+      [[2, 0], [2, 1], [2, 2]],
+      // Columns
+      [[0, 0], [1, 0], [2, 0]],
+      [[0, 1], [1, 1], [2, 1]],
+      [[0, 2], [1, 2], [2, 2]],
+      // Diagonals
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]],
+    ];
+  
+    return lines.some(checkLine);
+  };
+  
